9a1bbd25dde678621ff1bdd51d24f081dbd4a591
wkz <wangkangzhen@juxinda360.com>
2019-04-29 16:37:23 +0800

    FirstCommit 微信小程序底部导航栏
    Signed-off-by: 流沙 <wkz123011@gmail.com>

---
+++ page/Home/index.js
@@ -0,0 +1,75 @@
+// page/Home/Home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperCurrent: 0,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 800,
+    circular: true,
+    imgUrls: [
+'https://jxd360-service.oss-cn-beijing.aliyuncs.com/productapp/42da8967-ba2a-4da5-b4b6-0f50664a32aa',
+      'https://jxd360-service.oss-cn-beijing.aliyuncs.com/productapp/c211b99c-05fb-4b1c-9dfe-ba9e08da94fb'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
